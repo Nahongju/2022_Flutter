4f04e93762184356b99f8ea7ebdb86440dd66731
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-04-08 15:27:32 +0900

    Create fontapp_202.dart

---
+++ 202/08_fontApp/fontapp_202.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/* 어플리케이션에 폰트 추가하기
+  1. font 파일 준비하기 (파일명은 반드시 영어여야 함)
+  2. 프로젝트에 font 파일 생성
+  3. 미리 준비한 폰트를 프로젝트 내의 font 파일 안으로 드래그해서 가져오기
+  4. pubspec.yaml에 프로젝트 내부로 가지고 들어온 폰트 파일 추가
+  5. main.dart에 추가된 폰트 사용
+*/
