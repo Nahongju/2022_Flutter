bfee0c0f922972e511919c26f1711ad404ea78f8
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-04-08 15:22:46 +0900

    Create imageApp_main_201.dart

---
+++ 201/07. imageApp/imageApp_main_201.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+/* 앱에 이미지 추가하기
+  1. 앱에 추가할 이미지를 .png 파일 또는 .jpg로 준비
+  2. 프로젝트 내에 image 폴더 생성
+  3. 생성된 image 폴더에 준비한 이미지 넣기
+  4. main.dart 코드와 pubspec.yaml 코드 수정
+*/
+
+void main(){
+  runApp(ImageWidgetApp());
+}
+
+class ImageWidgetApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _ImageWidgetApp();
+  }
+}
+
+class _ImageWidgetApp extends State<ImageWidgetApp>{
+  @override
+  Widget build(BuildContext){
+    return Scaffold(
+      appBar: AppBar(title: Text('Image Widget')),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              // asset에 추가된 이미지를 위젯으로 가져오기
+              Image.asset('image/image.png')
+            ]
+          )     // Column
+        )     // Center
+      )      // Container
+    );      // Scaffold
+  }
+}
