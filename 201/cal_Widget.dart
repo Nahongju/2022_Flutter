a8788aed1773b2bebc9e73744fce2e8bb9823518
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-04-11 10:09:06 +0900

    Create cal_Widget.dart

---
+++ 201/cal_Widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  static const String _title = 'Cal Widget';
+  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: _title,
+      home: WidgetApp(),
+    );
+  }
+}
+
+class WidgetApp extends StatefulWidget{
+  @override
+  _WidgetExample createState() => _WidgetExample();
+}
+
+class _WidgetExample extends State<WidgetApp>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cal Widget'),
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}
