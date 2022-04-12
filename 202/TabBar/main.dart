ad4b9e3ecfab69f8d3e749c00936adee322e28b2
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-04-12 09:30:15 +0900

    Create main.dart

---
+++ 202/TabBar/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyHomePageState());
+}
+
+class MyHomePageState extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePageState>{
+  @override
+  Widget build(BuildContext context){
+    return null;
+  }
+}
