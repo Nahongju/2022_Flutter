e1995b6835d72c6fbb99449dac9e5dc3cabbbc42
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-04-13 10:53:58 +0900

    Update 09.cal_Widget_201.dart

--- 201/09.cal_Widget_201.dart
+++ 201/09.cal_Widget_201.dart
@@ -72,13 +72,33 @@ class _WidgetExample extends State<WidgetApp>{
                   child: Row(
                     children: <Widget> [
                       Icon(Icons.add),
-                      Text('더하기')
+                      Text('$_buttonText')
                     ],
                   ),
                   color: Colors.amber,
                   onPressed: () {
                     setState(() {
-                      int result = int.parse(value1.value.text) + int.parse(value2.value.text);
+                      var result;
+                      var value1Int = double.parse(value1.value.text);
+                      var value2Int = double.parse(value2.value.text);
+
+                      switch(_buttonText){
+                        case '더하기':
+                          result = value1Int + value2Int;
+                          break;
+
+                        case '빼기':
+                          result = value1Int - value2Int;
+                          break;
+
+                        case '곱하기':
+                          result = value1Int * value2Int;
+                          break;
+
+                        case '나누기':
+                          result = value1Int / value2Int;
+                          break;
+                      }
                       sum = '$result';
                     });
                   },
