387c171f460c239d23cbc311f0c07b22bbe8c754
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-05-02 11:41:45 +0900

    Update main.dart

--- 201/10_TabBar/main.dart
+++ 201/10_TabBar/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import './sub/firstPage.dart';
+import './sub/secondPage.dart';
+import './animalItem.dart';
 
 void main(){
   runApp(MyHomePage());
@@ -11,9 +14,35 @@ class MyHomePage extends StatefulWidget{
   }
 }
 
-class _MyHomePage extends State<MyHomePage>{
+class _MyHomePage extends State<MyHomePage>
+with SingleTickerProviderStateMixin{
+  TabController? controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dipose(){
+    controller!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
-    return null;
+    return Scaffold(
+      appBar: AppBar(title: Text('TabBar Ex'),),
+      body: TabBarView(
+        children: <Widget> [FirstApp(), SecondApp()],
+        controller: controller,
+      ),
+      bottomNavigationBar: TabBar(
+        tabs: <Tab> [
+          Tab(icon: Icon(Icons.looks_one, color: Colors.blue)),
+          Tab(icon: Icon(Icons.looks_two, color: Colors.blue,),)
+    ], controller: controller),
+    );
   }
 }
