93a2cc84eb289a57927d4ec3c113964bba5cb131
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-06-07 10:50:57 +0900

    Create student.dart

---
+++ 202/12_testCode/student.dart
@@ -0,0 +1,15 @@
+class Student{
+  String? school;
+  String? name;
+  String? grade;
+  String? Gclass;
+  String? number;
+
+  Student({
+    required this.school,
+    required this.name,
+    required this.grade,
+    required this.Gclass,
+    required this.number
+  });
+}
