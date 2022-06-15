cc47c9b308ebf0f84810bceda193d55676dd3e82
NaHongJu <44049699+Nahongju@users.noreply.github.com>
2022-06-15 10:59:45 +0900

    Update imagedownload.dart

--- 202/14. url로 이미지 다운로드/imagedownload.dart
+++ 202/14. url로 이미지 다운로드/imagedownload.dart
@@ -9,18 +9,31 @@ class ImageDownloadMain extends StatefulWidget{
 }
 
 class _ImageDownloadMain extends State<ImageDownloadMain>{
-  // 내려받을 이미지의 web 주소
-  final url = 'https://images.pexels.com/photos/240040/'
-      'pexels-photo-240040.jpeg?auto=compress';
+  // 내려받을 이미지의 주소를 입력받을 controller
+  TextEditingController? _editingController;
   bool downloading = false;     // 파일이 다운로드 중인지 확인
   var progressString = "";      // 다운로드가 어느정도 진행되었는지 저장
   String file = "";           // 파일의 내부저장소 위치
 
   @override
+  void initState(){
+    super.initState();
+    _editingController = new TextEditingController(
+      text: 'https://images.pexels.com/photos/3334482/'
+          'pexels-photo-3334482.jpeg?auto=compress'
+    );
+  }
+
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('image download'),
+        title: TextField(
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: 'url을 입력하세요'),
+        ),
       ),
       body: Center(
         child: downloading?
@@ -37,9 +50,36 @@ class _ImageDownloadMain extends State<ImageDownloadMain>{
               ], mainAxisAlignment: MainAxisAlignment.center,
             ),
           ),
-        ) 
-        : // downloading이 false일 경우
-        Container(),
+        ) : // downloading이 false일 경우
+         FutureBuilder(
+           builder: (context, snapshot) {
+             switch(snapshot.connectionState){
+               // FutureBuilder.future가 null 일 때
+               case ConnectionState.none:
+                 print('none');
+                 return Text('데이터 없음');
+
+               // 연결되기 전(FutureBuilder.future에서 데이터를 반환받지 않았을 때)
+               case ConnectionState.waiting:
+                 print('waiting');
+                 return CircularProgressIndicator();
+
+               // 하나 이상의 데이터를 반환받을 때
+               case ConnectionState.active:
+                 print('active');
+                 return CircularProgressIndicator();
+
+               // 모든 데이터를 받아서 연결이 끝날 때
+               case ConnectionState.done:
+                 print('done');
+                 if(snapshot.hasData){
+                   return snapshot.data as Widget;
+                 }
+             }
+             print('end process');
+             return Text('데이터 없음');
+           }, future: downloadWidget(file),
+         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () { downloadFile(); },
@@ -56,7 +96,7 @@ class _ImageDownloadMain extends State<ImageDownloadMain>{
       var dir = await getApplicationDocumentsDirectory();
       
       // 파일 다운로드 받기
-      await dio.download(url, '${dir.path}/myimage.jpg',
+      await dio.download(_editingController!.value.text, '${dir.path}/myimage.jpg',
       onReceiveProgress: (rec, total){
         print('Rec : $rec, Total : $total');
         file = '${dir.path}/myimage.jpg';
@@ -77,5 +117,21 @@ class _ImageDownloadMain extends State<ImageDownloadMain>{
       progressString = 'Completed';
     });
     print('Download Completed');
+  }
+
+  Future<Widget> downloadWidget(String filePath) async{
+    File file = File(filePath);
+    bool exist = await file.exists();
+
+    if(exist) {
+      return Center(
+        child: Column(
+          children: <Widget> [ Image.file(File(filePath)) ],
+        ),
+      );
+    }
+    else {
+      return Text('No Data');
+    }
   }
 }
